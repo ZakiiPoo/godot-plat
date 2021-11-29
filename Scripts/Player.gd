7be05d064aa552e69dfc86ca373dc3197ead849c
Zakii Poo <singh94564@gmail.com>
2021-11-29 00:38:49 -0800

    Player can now move I need to add levels and coins next

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,19 +1,60 @@
 extends KinematicBody2D
 
-export var gravity = 12000
-export var speed = 10100
-var jump_speed = 50000
+export (int) var gravity = 1000
+export (int) var speed = 200
+export (int) var jump_speed = -380
+
+var velocity = Vector2.ZERO
+
+var jumped = false
+
+# press sprint to speed up a bit
+func sprint():
+	if Input.is_action_pressed("sprint"):
+		speed = 300
+	else:
+		speed = 200
+
 func _ready():
 	$AnimatedSprite.animation = "idle"
 	$AnimatedSprite.playing = true
-	
-func getVelocity():
-	var veliocity = Vector2.ZERO
-	return veliocity
+
+# gets direction (this bit is a huge mess, but it works)
+func get_direction():
+	velocity.x = 0
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += speed
+		if !jumped:
+			$AnimatedSprite.animation = "run"
+		$AnimatedSprite.flip_h = false
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= speed
+		if !jumped:
+			$AnimatedSprite.animation = "run"
+		$AnimatedSprite.flip_h = true
+	if velocity.x == 0 && is_on_floor() && !jumped:
+		$AnimatedSprite.animation = "idle"
+
 func fall():
-	if !is_on_floor():
+	if !jumped && !is_on_floor():
 		$AnimatedSprite.animation = "fall"
 
+func jump():
+	if Input.is_action_pressed("ui_up"):
+		if is_on_floor():
+			velocity.y = jump_speed
+			$AnimatedSprite.animation = "jump"
+			jumped = true
+			$JumpTimer.start()
+
 func _process(delta):
-	var velocity = getVelocity()
-	move_and_slide(velocity * delta, Vector2.UP)
+	get_direction()
+	sprint()
+	velocity.y += gravity * delta
+	velocity = move_and_slide(velocity, Vector2.UP)
+	jump()
+	fall()
+
+# used a timer so the fall animation wouldn't play
+func _on_JumpTimer_timeout():
+	jumped = false
